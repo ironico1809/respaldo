d14ac9646edf7491c65ec1c9519d49cb7faab665
ironico1809 <jersonalexander577@gmail.com>
2025-10-28 15:28:18 -0400

    primer avance del examen

--- backend/nucleo/wsgi.py
+++ backend/nucleo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Api_2doParcial project.
+WSGI config for nucleo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Api_2doParcial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nucleo.settings')
 
 application = get_wsgi_application()
